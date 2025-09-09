7f97b9e4f57b87f7afcd197d36a8d54da217f8d5
Abdul-Majeed Nurudeen <abdulmajeednurudeen48@gmail.com>
2025-09-09 16:23:05 +0000

    Updated prisma client config

--- src/config/prismaClient.js
+++ src/config/prismaClient.js
@@ -1,4 +1,9 @@
-import { PrismaClient } from "@/generated/prisma";
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis;
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
